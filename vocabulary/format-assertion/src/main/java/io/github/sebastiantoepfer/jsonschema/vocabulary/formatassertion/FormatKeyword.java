6dc7a0941b2cba7ca4f2269756b5175fe98c04f8
Sebastian Toepfer <61313468+sebastian-toepfer@users.noreply.github.com>
2024-12-02 21:43:58 +0100

    add format assertion for dates,times and duration

---
+++ vocabulary/format-assertion/src/main/java/io/github/sebastiantoepfer/jsonschema/vocabulary/formatassertion/FormatKeyword.java
@@ -0,0 +1,74 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2024 sebastian.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package io.github.sebastiantoepfer.jsonschema.vocabulary.formatassertion;
+
+import io.github.sebastiantoepfer.ddd.common.Media;
+import io.github.sebastiantoepfer.jsonschema.InstanceType;
+import io.github.sebastiantoepfer.jsonschema.keyword.Annotation;
+import io.github.sebastiantoepfer.jsonschema.keyword.Assertion;
+import jakarta.json.Json;
+import jakarta.json.JsonString;
+import jakarta.json.JsonValue;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+
+final class FormatKeyword implements Assertion, Annotation {
+
+    static final String NAME = "format";
+    private final Formats.Format format;
+
+    FormatKeyword(final Formats.Format format) {
+        this.format = Objects.requireNonNull(format);
+    }
+
+    @Override
+    public Collection<KeywordCategory> categories() {
+        return List.of(KeywordCategory.ASSERTION, KeywordCategory.ANNOTATION);
+    }
+
+    @Override
+    public boolean isValidFor(final JsonValue instance) {
+        return !InstanceType.STRING.isInstance(instance) || hasValidValue((JsonString) instance);
+    }
+
+    private boolean hasValidValue(final JsonString value) {
+        return format.applyTo(value.getString());
+    }
+
+    @Override
+    public boolean hasName(final String name) {
+        return NAME.equals(name);
+    }
+
+    @Override
+    public JsonValue valueFor(final JsonValue value) {
+        return Json.createValue(format.name());
+    }
+
+    @Override
+    public <T extends Media<T>> T printOn(final T media) {
+        return media.withValue(NAME, format.name());
+    }
+}
---
+++ vocabulary/format-assertion/src/test/java/io/github/sebastiantoepfer/jsonschema/vocabulary/formatassertion/FormatsTest.java
@@ -0,0 +1,62 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2024 sebastian.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package io.github.sebastiantoepfer.jsonschema.vocabulary.formatassertion;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class FormatsTest {
+
+    @Test
+    void should_return_unknown() {
+        assertThat(new Formats().findByName("unknow").name(), is("unknow"));
+        assertThat(new Formats().findByName("unknow").applyTo("value"), is(false));
+    }
+
+    @Test
+    void should_found_dateTimeFormat() {
+        assertThat(new Formats().findByName("date-time").applyTo("2024-11-29T00:05:00+01:00"), is(true));
+        assertThat(new Formats().findByName("date-time").applyTo("noDateTime"), is(false));
+    }
+
+    @Test
+    void should_found_dateFormat() {
+        assertThat(new Formats().findByName("date").applyTo("2024-11-29"), is(true));
+        assertThat(new Formats().findByName("date").applyTo("noDate"), is(false));
+    }
+
+    @Test
+    void should_found_timeFormat() {
+        assertThat(new Formats().findByName("time").applyTo("00:05:00.0182Z"), is(true));
+        assertThat(new Formats().findByName("time").applyTo("noTime"), is(false));
+    }
+
+    @Test
+    void should_found_durationFormat() {
+        assertThat(new Formats().findByName("duration").applyTo("PT1H10M"), is(true));
+        assertThat(new Formats().findByName("duration").applyTo("noDuration"), is(false));
+    }
+}
